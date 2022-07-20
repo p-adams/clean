f0fd2b3b76f943257e4146936da98ded25b52554
Paul Adams <ghusiyy@gmail.com>
2022-07-20 00:43:28 -0700

    add css parser package

--- lib/clean.ts
+++ lib/clean.ts
@@ -1,4 +1,7 @@
+import * as css from "https://deno.land/x/css@0.3.0/mod.ts";
+
 export async function clean() {
-  const styleSheet = await Deno.readTextFile(Deno.env.get("VAR") as string);
-  console.log(styleSheet);
+  const styleSheet = await Deno.readTextFile("../data/index.css");
+  const parsedCss = css.parse(styleSheet);
+  console.log(parsedCss);
 }
